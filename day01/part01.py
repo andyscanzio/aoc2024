e562f90675a56178c5a28c07d289118e3786a5bf
Andrea Scanzio <48473898+andyscanzio@users.noreply.github.com>
2024-12-01 22:16:24 +0100

    Day 01

---
+++ day01/part01.py
@@ -0,0 +1,8 @@
+with open('input.txt', 'r') as file:
+    lines = [i.removesuffix('\n').split('   ') for i in file.readlines()]
+
+left = sorted([int(x) for (x, _) in lines])
+right = sorted([int(x) for (_, x) in lines])
+
+res = sum(abs(a - b) for (a, b) in zip(left, right))
+print(res)
---
+++ day01/part02.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+with open('input.txt', 'r') as file:
+    lines = [i.removesuffix('\n').split('   ') for i in file.readlines()]
+    
+left = [int(x) for (x, _) in lines]
+right = Counter([int(x) for (_, x) in lines])
+
+res = sum(l * right[l] for l in left)
+print(res)
