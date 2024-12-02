2e42a324d0459957518004f8b7ac7bf681e5b193
Andrea Scanzio <48473898+andyscanzio@users.noreply.github.com>
2024-12-02 22:49:51 +0100

    Day 2

---
+++ day02/part01.py
@@ -0,0 +1,19 @@
+from itertools import pairwise
+
+def is_safe(line:list[int])->bool:
+    if line[1] == line[0]:
+        return False
+    sign = (line[1] - line[0])//abs(line[1] - line[0])
+    for a, b in pairwise(line):
+        if b == a:
+            return False
+        if (b-a)//abs(b-a) != sign:
+            return False
+        if abs(b-a) > 3:
+            return False
+    return True
+    
+
+with open('input.txt', 'r') as file:        
+    res = sum(is_safe(list(map(int, line.rstrip('\n').split(' ')))) for line in file)
+print(res)
---
+++ day02/part02.py
@@ -0,0 +1,27 @@
+from itertools import pairwise
+
+def is_safe(line:list[int])->bool:
+    if line[1] == line[0]:
+        return False
+    sign = (line[1] - line[0])//abs(line[1] - line[0])
+    for a, b in pairwise(line):
+        if b == a:
+            return False
+        if (b-a)//abs(b-a) != sign:
+            return False
+        if abs(b-a) > 3:
+            return False
+    return True
+    
+def is_safe_damp(line:list[int])->bool:
+    if is_safe(line):
+        return True
+    for i in range(len(line)):
+        temp = [x for j, x in enumerate(line) if j != i]
+        if is_safe(temp):
+            return True
+    return False
+
+with open('input.txt', 'r') as file:        
+    res = sum(is_safe_damp(list(map(int, line.rstrip('\n').split(' ')))) for line in file)
+print(res)
