9b968662bd0011ba0a69b6d72e3d9a7c6181c967
Andrea Scanzio <48473898+andyscanzio@users.noreply.github.com>
2024-12-04 21:32:24 +0100

    Day3

---
+++ day03/part01.py
@@ -0,0 +1,10 @@
+import re
+
+pattern = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
+
+with open('input.txt', 'r') as file:
+    text = file.read()
+
+match = pattern.findall(text)
+res = sum(int(a) * int(b) for (a, b) in match)
+print(res)
---
+++ day03/part02.py
@@ -0,0 +1,18 @@
+import re
+
+pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don\'t)\(\)")
+
+with open('input.txt', 'r') as file:
+    text = file.read()
+
+match = pattern.findall(text)
+do_mult = True
+res = 0
+for a, b, do, dont in match:
+    if do:
+        do_mult = True
+    elif dont:
+        do_mult = False
+    elif do_mult:
+        res += int(a)*int(b)
+print(res)
