97398eb14561e3df4c8655ea9c2d4d38b48f64d6
Andrea Scanzio <48473898+andyscanzio@users.noreply.github.com>
2024-12-11 21:33:05 +0100

    Day 11

---
+++ day11/part01.py
@@ -0,0 +1,24 @@
+from tqdm import tqdm
+
+
+test1 = '0 1 10 99 999'
+test2 = '125 17'
+input = '8793800 1629 65 5 960 0 138983 85629'
+
+stones = list(map(int, input.split(' ')))
+print(stones)
+
+for _ in tqdm(range(25)):
+    temp = []
+    for stone in stones:
+        if stone == 0:
+            temp.append(1)
+        elif len(str(stone)) % 2 == 0:
+            s = str(stone)
+            l = len(s)
+            a, b = s[0:l//2], s[l//2:]
+            temp.extend((int(a), int(b)))
+        else:
+            temp.append(2024 * stone)
+    stones = temp
+print(len(stones))
